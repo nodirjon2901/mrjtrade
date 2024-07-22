4ef13770704a60d9acf5a3fdfa1faabc50ade300
SuperHuma <theferganes@gmail.com>
2024-07-22 17:28:28 +0500

    banner update

--- src/main/java/org/example/mrj/domain/BannerWrapper.java
+++ src/main/java/org/example/mrj/domain/BannerWrapper.java
@@ -2,8 +2,6 @@ package org.example.mrj.domain;
 
 import org.example.mrj.domain.entity.Banner;
 
-import java.io.Serializable;
-
-public record BannerWrapper(Banner banner) implements Serializable
+public record BannerWrapper(Banner banner)
 {
 }
