8e0d46416a38e83ccd9b1747df29308413a71b6b
nodirjon2901 <nodirjontojiahmedov2901@gmail.com>
2024-07-29 15:19:12 +0500

    Commit by Nodir. NewSer

--- src/main/java/org/example/mrj/service/NewService.java
+++ src/main/java/org/example/mrj/service/NewService.java
@@ -29,8 +29,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class NewService
-{
+public class NewService {
 
     private final NewRepository newRepository;
 
@@ -40,18 +39,15 @@ public class NewService
 
     private final ObjectMapper objectMapper;
 
-    public ResponseEntity<ApiResponse<New>> create(String strNew, MultipartHttpServletRequest request)
-    {
+    public ResponseEntity<ApiResponse<New>> create(String strNew, MultipartHttpServletRequest request) {
         ApiResponse<New> response = new ApiResponse<>();
         Optional<Integer> maxOrderNum = newRepository.getMaxOrderNum();
-        try
-        {
+        try {
             New newness = objectMapper.readValue(strNew, New.class);
             newness.setOrderNum(maxOrderNum.map(num -> num + 1).orElse(1));
             newness.setActive(true);
             Iterator<String> fileNames = request.getFileNames();
-            while (fileNames.hasNext())
-            {
+            while (fileNames.hasNext()) {
                 String key = fileNames.next();
                 MultipartFile photo = request.getFile(key);
                 setNewsPhoto(key, photo, newness);
@@ -65,17 +61,14 @@ public class NewService
             System.err.println("save.getHead() = " + save.getHead());
 
             return ResponseEntity.ok().body(response);
-        } catch (JsonProcessingException e)
-        {
+        } catch (JsonProcessingException e) {
             response.setMessage(e.getMessage());
             return ResponseEntity.status(409).body(response);
         }
     }
 
-    private void setNewsPhoto(String key, MultipartFile photo, New newness)
-    {
-        if (key.equalsIgnoreCase("main-photo"))
-        {
+    private void setNewsPhoto(String key, MultipartFile photo, New newness) {
+        if (key.equalsIgnoreCase("main-photo")) {
             newness.getHead().setPhoto(photoService.save(photo));
             return;
         }
@@ -84,12 +77,10 @@ public class NewService
         newOption.setPhoto(photoService.save(photo));
     }
 
-    public ResponseEntity<ApiResponse<New>> findById(Long id)
-    {
+    public ResponseEntity<ApiResponse<New>> findById(Long id) {
         ApiResponse<New> response = new ApiResponse<>();
         Optional<New> optionalNew = newRepository.findById(id);
-        if (optionalNew.isEmpty())
-        {
+        if (optionalNew.isEmpty()) {
             response.setMessage("New is not found by id: " + id);
             return ResponseEntity.status(404).body(response);
         }
@@ -99,12 +90,10 @@ public class NewService
         return ResponseEntity.status(200).body(response);
     }
 
-    public ResponseEntity<ApiResponse<New>> findBySlug(String slug)
-    {
+    public ResponseEntity<ApiResponse<New>> findBySlug(String slug) {
         ApiResponse<New> response = new ApiResponse<>();
         Optional<New> optionalNew = newRepository.findBySlug(slug);
-        if (optionalNew.isEmpty())
-        {
+        if (optionalNew.isEmpty()) {
             response.setMessage("New is not found by slug: " + slug);
             return ResponseEntity.status(404).body(response);
         }
@@ -114,8 +103,7 @@ public class NewService
         return ResponseEntity.status(200).body(response);
     }
 
-    public ResponseEntity<ApiResponse<List<NewDTO>>> findAll()
-    {
+    public ResponseEntity<ApiResponse<List<NewDTO>>> findAll() {
         ApiResponse<List<NewDTO>> response = new ApiResponse<>();
         List<New> all = newRepository.findAll();
         response.setData(new ArrayList<>());
@@ -125,8 +113,7 @@ public class NewService
         return ResponseEntity.status(200).body(response);
     }
 
-    public ResponseEntity<ApiResponse<List<NewDTO>>> findAllByPageNation(int page, int size)
-    {
+    public ResponseEntity<ApiResponse<List<NewDTO>>> findAllByPageNation(int page, int size) {
         ApiResponse<List<NewDTO>> response = new ApiResponse<>();
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<New> all = newRepository.findAll(pageable);
@@ -135,8 +122,7 @@ public class NewService
         return ResponseEntity.status(200).body(response);
     }
 
-    public ResponseEntity<ApiResponse<List<NewDTO>>> findOtherFourNews(String newSlug)
-    {
+    public ResponseEntity<ApiResponse<List<NewDTO>>> findOtherFourNews(String newSlug) {
         ApiResponse<List<NewDTO>> response = new ApiResponse<>();
         List<New> all = newRepository.findAllByOrderByIdAsc();
         response.setData(new ArrayList<>());
@@ -147,119 +133,16 @@ public class NewService
         return ResponseEntity.status(200).body(response);
     }
 
-    public ResponseEntity<ApiResponse<New>> update(New newness)
-    {
-        System.err.println("newness.getHead() = " + newness.getHead());
-
-        ApiResponse<New> response = new ApiResponse<>();
-        try
-        {
-            New fromDB = newRepository.findById(newness.getId())
-                    .orElseThrow(() -> new NotFoundException("New is not found by id: " + newness.getId()));
-
-            if (newness.getHead() != null)
-            {
-                NewHeadOption newHead = newness.getHead();
-
-                if (newHead.getTitle() != null)
-                {
-                    fromDB.getHead().setTitle(newHead.getTitle());
-                    String slug = fromDB.getId() + "-" + SlugUtil.makeSlug(newHead.getTitle());
-                    fromDB.setSlug(slug);
-                }
-
-                if (newHead.getBody() != null)
-                {
-                    fromDB.getHead().setBody(newHead.getBody());
-                }
-            }
-
-            if (newness.getNewOptions() != null)
-            {
-                List<NewOption> fromDBOptions = fromDB.getNewOptions();
-                List<NewOption> newOptions = newness.getNewOptions();
-
-                Map<Long, NewOption> existingOptionsMap = fromDBOptions.stream()
-                        .collect(Collectors.toMap(NewOption::getId, option -> option));
-
-                List<NewOption> toRemove = new ArrayList<>();
-                for (NewOption newOption : newOptions)
-                {
-                    if (newOption.getId() != null)
-                    {
-                        NewOption existingOption = existingOptionsMap.get(newOption.getId());
-                        if (newOption.getHeading() == null && newOption.getText() == null)
-                        {
-                            toRemove.add(existingOption);
-                        }
-                    }
-                }
-
-                fromDBOptions.removeAll(toRemove);
-                newOptions.removeAll(toRemove);
-
-                List<NewOption> updatedOptions = new ArrayList<>();
-
-                for (int i = 0; i < newOptions.size(); i++)
-                {
-                    NewOption newOption = newOptions.get(i);
-
-                    if (newOption.getId() != null)
-                    {
-                        NewOption existingOption = existingOptionsMap.get(newOption.getId());
-                        if (existingOption != null)
-                        {
-                            if (newOption.getHeading() != null)
-                            {
-                                existingOption.setHeading(newOption.getHeading());
-                            }
-                            if (newOption.getText() != null)
-                            {
-                                existingOption.setText(newOption.getText());
-                            }
-                            existingOption.setOrderNum(i + 1);
-                            updatedOptions.add(existingOption);
-                        }
-                    } else
-                    {
-                        newOption.setOrderNum(i + 1);
-                        updatedOptions.add(newOption);
-                    }
-                }
-
-                fromDB.getNewOptions().clear();
-                fromDB.getNewOptions().addAll(updatedOptions);
-            }
-
-            if (newness.getActive() != null)
-            {
-                fromDB.setActive(newness.getActive());
-            }
-
-            response.setMessage("Updated");
-            response.setData(newRepository.save(fromDB));
-            return ResponseEntity.status(201).body(response);
-        } catch (Exception e)
-        {
-            response.setMessage("Error: " + e.getMessage());
-            return ResponseEntity.status(400).body(response);
-        }
-    }
-
-
-    public ResponseEntity<ApiResponse<New>> update2(New newness)
-    {
+    public ResponseEntity<ApiResponse<New>> update2(New newness) {
 
         ApiResponse<New> response = new ApiResponse<>();
         New fromDB = newRepository.findById(newness.getId())
                 .orElseThrow(() -> new NotFoundException("New is not found by id: " + newness.getId()));
 
-        if (newness.getHead() != null)
-        {
+        if (newness.getHead() != null) {
             NewHeadOption newHead = newness.getHead();
 
-            if (newHead.getTitle() != null)
-            {
+            if (newHead.getTitle() != null) {
                 fromDB.getHead().setTitle(newHead.getTitle());
             }
 
@@ -267,35 +150,30 @@ public class NewService
                 fromDB.getHead().setBody(newHead.getBody());
         }
 
-        if (newness.getNewOptions() != null)
-        {
+        if (newness.getNewOptions() != null) {
             List<NewOption> fromDBOptions = fromDB.getNewOptions();
             List<NewOption> newOptions = newness.getNewOptions();
 
-            for (NewOption newOption : newOptions)
-            {
-
-                for (NewOption fromDBOption : fromDBOptions)
-                {
-                    if (newOption.getId() != null && newOption.getId().equals(fromDBOption.getId()))
-                    {
+            for (NewOption newOption : newOptions) {
+                int i = 1;
+                for (NewOption fromDBOption : fromDBOptions) {
+                    if (newOption.getId() != null && newOption.getId().equals(fromDBOption.getId())) {
                         if (newOption.getHeading() != null) fromDBOption.setHeading(newOption.getHeading());
                         if (newOption.getText() != null) fromDBOption.setText(newOption.getText());
-
-                        if (newOption.getText() == null && newOption.getHeading() == null)
-                        {
+                        fromDBOption.setOrderNum(i);
+                        if (newOption.getText() == null && newOption.getHeading() == null) {
                             System.err.println("newOption.getId() = " + newOption.getId());
                             newOptionRepository.deleteee(newOption.getId());
                         }
                     }
                 }
 
-                if (newOption.getId() == null)
-                {
+                if (newOption.getId() == null) {
+                    newOption.setOrderNum(i);
                     newOption.setNewness(fromDB);
                     fromDBOptions.add(newOption);
                 }
-
+                i++;
             }
         }
         response.setData(newRepository.save(fromDB));
@@ -304,23 +182,18 @@ public class NewService
     }
 
 
-    public ResponseEntity<ApiResponse<List<NewDTO>>> changeOrder(List<NewnessWrapper> newnessWrapperList)
-    {
+    public ResponseEntity<ApiResponse<List<NewDTO>>> changeOrder(List<NewnessWrapper> newnessWrapperList) {
         ApiResponse<List<NewDTO>> response = new ApiResponse<>();
         response.setData(new ArrayList<>());
         List<New> dbAll = newRepository.findAll();
-        if (dbAll.size() != newnessWrapperList.size())
-        {
+        if (dbAll.size() != newnessWrapperList.size()) {
             response.setMessage("In database have: " + dbAll.size() + " newness. But you send " + newnessWrapperList.size() + " order number(s)");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
-        for (New db : newRepository.findAll())
-        {
+        for (New db : newRepository.findAll()) {
             Long dbId = db.getId();
-            for (NewnessWrapper newnessWrapper : newnessWrapperList)
-            {
-                if (newnessWrapper.id().equals(dbId))
-                {
+            for (NewnessWrapper newnessWrapper : newnessWrapperList) {
+                if (newnessWrapper.id().equals(dbId)) {
                     db.setOrderNum(newnessWrapper.orderNum());
                     response.getData().add(new NewDTO(newRepository.save(db)));
                 }
@@ -330,11 +203,9 @@ public class NewService
         return ResponseEntity.status(201).body(response);
     }
 
-    public ResponseEntity<ApiResponse<?>> deleteById(Long id)
-    {
+    public ResponseEntity<ApiResponse<?>> deleteById(Long id) {
         ApiResponse<?> response = new ApiResponse<>();
-        if (newRepository.findById(id).isEmpty())
-        {
+        if (newRepository.findById(id).isEmpty()) {
             response.setMessage("New is not found by id: " + id);
             return ResponseEntity.status(404).body(response);
         }
